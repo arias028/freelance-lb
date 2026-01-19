50bacf71fb734d135762bde0107bc11e28b5170c
arias028 <syahrulpamungkas605@gmail.com>
2026-01-19 10:02:42 +0700

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -87,12 +87,12 @@ export default defineNuxtConfig({
       orientation: 'portrait',
       icons: [
         {
-          src: 'pwa-192x192.png',
+          src: '/pwa-192x192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'pwa-512x512.png',
+          src: '/pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable'
